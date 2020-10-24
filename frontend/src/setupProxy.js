5e15ced027adcc821b86f24718b3e4083a5825cd
bkerdzaia <b.kerdzaia@gmail.com>
2020-10-24 22:25:15 +0400

    fix proxy bugs

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy(["/api", , "/api"], { target: "http://backend:8000" })
+    proxy.createProxyMiddleware(["/api", , "/api"], { target: "http://backend:8000" })
   );
 };
